a4d55267bc874615a08eb3b307d95e4df225b8e0
Psycho90 <maximsh90@mail.ru>
2018-02-05 10:34:06 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,10 +1,11 @@
 $(document).ready(function(){
+    svg4everybody();
     $('.news').slick({
-        prevArrow:"<svg xmlns=\"http://www.w3.org/2000/svg\" role=\"presentation\" class=\"news__arrow news__arrow_left\">\n" +
-        "                    <use xlink:href=\"img/icons/arrow-left.svg#left\"></use>\n" +
+        prevArrow:"<svg xmlns=\"http://www.w3.org/2000/svg\" viewBox=\"0 0 588.3 1004.6\" width='15' height='25' role=\"presentation\" class=\"news__arrow news__arrow_left\">\n" +
+        "                    <use xlink:href=\"/img/icons/arrow-left.svg#left\"></use>\n" +
         "                </svg>",
-        nextArrow:"<svg xmlns=\"http://www.w3.org/2000/svg\" role=\"presentation\" class=\"news__arrow news__arrow_right\">\n" +
-        "                    <use xlink:href=\"img/icons/arrow-right.svg#right\"></use>\n" +
+        nextArrow:"<svg xmlns=\"http://www.w3.org/2000/svg\" viewBox=\"0 0 640 1052\" width='15' height='25' role=\"presentation\" class=\"news__arrow news__arrow_right\">\n" +
+        "                    <use xlink:href=\"/img/icons/arrow-right.svg#right\"></use>\n" +
         "                </svg>"
     });
 });
